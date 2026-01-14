243df59f2620dd37a67916ef1c7cb5a781fc2c4b
DanielRossiBR <daniel-ilha@hotmail.com>
2026-01-13 22:41:50 -0300

    projeto hero

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-console.log("Primeira MSG");
-console.log("Teste");
+// 1. Criando as variáveis para armazenar o nome e o XP
+let nomeHeroi = "Daniel";
+let xp = 2500;
+
+// 2. Estrutura de decisão para determinar o nível
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 6001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// 3. Exibindo a mensagem final
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
